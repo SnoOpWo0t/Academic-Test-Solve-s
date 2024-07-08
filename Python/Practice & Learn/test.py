8245c35ec7bf0e7b4df06f6d755d9bb67c167e83
Afrin Sultana Akhi <afrinsultanaakhi138@gmail.com>
2024-07-08 13:08:34 +0600

    k

--- Python/Practice & Learn/test.py
+++ Python/Practice & Learn/test.py
@@ -1,8 +1,16 @@
-a = 6
-b = 9
-if a == 5 :
-    print("hello")
-elif b== 7:
-    print ("world")
-else:
-    print("bye")
+stud = {
+    "akhi" :0,
+    "sai" : 80,
+    "faial" :90,
+    "fahad" : 900
+}
+def fun(stud):
+    highest_scores = 0
+    scores = 0
+    for i in stud:
+        if stud[i] > scores :
+            scores = stud[i]
+            
+    highest_scores = scores
+    print(f"highest_scores is {highest_scores} and student name is {i}")
+fun(stud)
