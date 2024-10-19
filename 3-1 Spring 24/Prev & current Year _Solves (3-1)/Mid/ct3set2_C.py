88fb9ac34f73a0f89683532972c6b7e2ce409830
Afrin Sultana Akhi <afrinsultanaakhi138@gmail.com>
2024-10-19 18:08:14 +0600

    k

---
+++ 3-1 Spring 24/Prev & current Year _Solves (3-1)/Mid/ct3set2_C.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self, student_id , major):
+        self.student_id = student_id
+        self.major = major
+    def display_student_info(self):
+        print(f"{self.student_id} and {self.major}")
+class Employee :
+    def __init__(self,employee_name):
+        self.employee_name = employee_name
+class Manager(Employee) :
+    def __init__(self, employee_name,department):
+        super().__init__(employee_name)
+        self.department = department
+    def show_manager_details(self):
+        print(f"{self.employee_name} , {self.department}")
+    
--- 3-1 Spring 24/Previous_year _Solves (3-1)/Mid/sp21 q3.py
+++
@@ -1,6 +0,0 @@
-list = []
-n = int(input())
-for i in range (n) :
-    num = int(input())
-    list.append(num)
-    
